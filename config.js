73074b38be5046b438613f46c308d517caeae23e
adnan-codes <82684958+adnan-codes@users.noreply.github.com>
2021-04-17 02:37:47 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //CHANGE ALL OF THESE
+    channelid: "813181762602926150",
+    token: "ODExMzgwNjU5NDQzNDY2MjYx.YCxXHw.hE1Rlpu_U3saxLcctfTkGbn0wUo",
+    botname: "Loomi",
+
+    // API AUTHORIZATION
+    authorization: "NzMwMDE2MDk2NzA2NDk0NDc1.MTYxODYxMzYyNDk0NQ==.e90a4b420720338c234aa5b422d5e271"
+}
